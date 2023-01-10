da94ceb4e25d2f10b553dbc3efe3e0f40d8dc46b
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-01-10 17:19:41 +0800

    '

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-function myFunction(event){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
+$(document).ready(function(){
+    $("#submit").click(function(){
+        var username = document.getElementById("Username").value;
+        var password = document.getElementById("Password").value;
 
-    localStorage.setItem("Username", username);
-    localStorage.setItem("Password", password);
-}
+
+
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://interactivedev-30ef.restdb.io/rest/student",
+            "method": "GET",
+            "headers": {
+            "content-type": "application/json",
+            "x-apikey": "<your CORS apikey here>",
+            "cache-control": "no-cache"
+            }
+        }
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+        });
+
+        console.log(response);
+
+
+
+
+    })
+})
