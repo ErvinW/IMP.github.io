ee65e251ab3a2b53006ac6a8ded531bfd00659b0
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-01-11 13:40:06 +0800

    Added redirect to next page on successful login

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 localStorage.clear()
-for (let i = 1; i < 6; i++)
-{
-    let username = "Username" + i.toString()
-    let user = {
-    username: username,
-    password: "Password" + i.toString()
+// for (let i = 1; i < 6; i++)
+// {
+//     let username = "Username" + i.toString()
+//     let user = {
+//     username: username,
+//     password: "Password" + i.toString()
+//     }
+//     let json = JSON.stringify(user)
+//     localStorage.setItem(username, json);
+// }
+
+   class Student{
+    constructor(username, password, name, studentclass,  module1, module2, module3, GPA){
+        this.username = username
+        this.password = password
     }
-    let json = JSON.stringify(user)
-    localStorage.setItem(username, json);
 }
+
+
+let student1 = new Student("student1", "password1", "name1", "class1", 4.0, 4.0, 4.0,4.0);
+let json1 = JSON.stringify(student1)
+localStorage.setItem(student1.username,json1)
+
+let student2 = new Student("student2", "password2", "name2", "class1", 3.0, 3.0, 3.0,3.0);
+let json2 = JSON.stringify(student2)
+localStorage.setItem(student2.username,json2)
+
+let student3 = new Student("student3", "password3", "name3", "class1", 4.0, 4.0, 3.8,3.9);
+let json3 = JSON.stringify(student3)
+localStorage.setItem(student3.username,json3)
+
+let student4 = new Student("student4", "password4", "name4", "class1", 4.0, 2.5, 2.7,3.1);
+let json4 = JSON.stringify(student4)
+localStorage.setItem(student4.username,json4)
 
 
 document.querySelector("#submit").addEventListener("click", function(){   
@@ -19,8 +43,11 @@ document.querySelector("#submit").addEventListener("click", function(){
     var user = localStorage.getItem(Username)
     var data = JSON.parse(user)
     if(Username == data.username && Password == data.password){
-        document.querySelector("#form").setAttribute("action", "page2.html")
-        console.log(data);
+        window.location.assign("page2.html");
+        
+    }
+    else{
+        console.log("Error incorrect password or username");
     }
    })
 
@@ -28,27 +55,3 @@ document.querySelector("#submit").addEventListener("click", function(){
 
 
 
-//    class Student{
-//     constructor(username, password, name, studentclass,  module1, module2, module3, GPA){
-        
-//     }
-// }
-
-
-// let student1 = new Student("student1", "password1", "name1", "class1", 4.0, 4.0, 4.0,4.0);
-// let json1 = JSON.stringify(student1)
-// localStorage.setItem(student1.username,json1)
-
-// let student2 = new Student("student2", "password2", "name2", "class1", 3.0, 3.0, 3.0,3.0);
-// let json2 = JSON.stringify(student2)
-// localStorage.setItem(student2.username,json2)
-
-// let student3 = new Student("student3", "password3", "name3", "class1", 4.0, 4.0, 3.8,3.9);
-// let json3 = JSON.stringify(student3)
-// localStorage.setItem(student3.username,json3)
-
-// let student4 = new Student("student4", "password4", "name4", "class1", 4.0, 2.5, 2.7,3.1);
-// let json4 = JSON.stringify(student4)
-// localStorage.setItem(student4.username,json4)
-
-   
