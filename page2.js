c004c0ccf77298089637f531b614752ffa3ef13b
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-01-13 22:13:38 +0800

    .

--- page2.js
+++ page2.js
@@ -2,6 +2,11 @@
 
 const ElName = document.getElementById("Name");
 const ElClass = document.getElementById("class");
+const ElID = document.getElementById("ID");
 Eluser = localStorage.getItem("Username");
+var user = localStorage.getItem(localStorage.key(Eluser))
+var data = JSON.parse(user)
 ElName.innerHTML = "Welcome, " + Eluser;
+ElClass.innerHTML = "Class: " + data.class
+ElID.innerHTML = "Student ID: " + data.password
 
