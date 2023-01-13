8c11d5ae743ccfe54e4e6bcab62f6d6613b3004a
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-01-13 16:19:09 +0800

    .

--- page2.js
+++ page2.js
@@ -4,10 +4,4 @@ const ElName = document.getElementById("Name");
 const ElClass = document.getElementById("class");
 Eluser = localStorage.getItem("Username");
 ElName.innerHTML = "Welcome, " + Eluser;
-for (let i = 1; i < 9999999 ; i++){
-    if (Eluser == "student1"){
-        const ElNO = "student" + toString(i);
-        console.log(ElNO)
-    }
-}
-
+Eltest = document.getElementById("class");
