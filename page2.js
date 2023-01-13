c82667125daa320cb0b5af30d6840141b704759c
ErvinW <116048866+ErvinW@users.noreply.github.com>
2023-01-13 21:27:51 +0800

    .

--- page2.js
+++ page2.js
@@ -4,4 +4,4 @@ const ElName = document.getElementById("Name");
 const ElClass = document.getElementById("class");
 Eluser = localStorage.getItem("Username");
 ElName.innerHTML = "Welcome, " + Eluser;
-Eltest = document.getElementById("class");
+
